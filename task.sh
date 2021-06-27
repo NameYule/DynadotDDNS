65e1102ee610e74f82d06c7bf638345cbb1a44cd
Eric <yule.hurrah@gmail.com>
2021-06-27 14:02:02 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -3,5 +3,5 @@ RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 CMD="\"$RUN_DIR/dynadot.py\" >> \"$RUN_DIR/run.log\""
 
-echo "*/30 * * * *   root    $CMD" > /etc/cron.d/dynadot;
+echo "0 0 /2 * * ? root    $CMD" > /etc/cron.d/dynadot;
 /etc/init.d/cron reload;
